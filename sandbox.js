b7680884b85643e4baf183b68ed7c7b02564cb26
mizan <mizzanur007@gmail.com>
2020-04-04 13:18:50 +0600

    updated the callback with the promises

--- sandbox.js
+++ sandbox.js
@@ -1,35 +1,76 @@
 // 5. creating the callback function for reusable code :D
-const getData = (callback) => {
-  // 1. first creating the async request variable
-  const request = new XMLHttpRequest();
+const getData = (resource) => {
 
-  // 4. using addEventListener for tracking the state of the data using readyStateChange
-  request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText) // turning JSON into Object
-      callback(undefined, data);
-    } else if(request.readyState === 4) {
-      callback('could not fetch the data', undefined);
-    }
+  // creating a promise
+  return new Promise((resolve, reject) => {
+    // 1. first creating the async request variable
+    const request = new XMLHttpRequest();
+
+    // 4. using addEventListener for tracking the state of the data using readyStateChange
+    request.addEventListener('readystatechange', () => {
+      if(request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText) // turning JSON into Object
+        resolve(data);
+      } else if(request.readyState === 4) {
+        reject('could not fetch the data');
+      }
+    })
+
+    // 2. creating the open request ( what to do -- GET or POST )
+    request.open('GET', resource)
+
+    // 3. receiving the response
+    request.send()
   })
-
-  // 2. creating the open request ( what to do -- GET or POST )
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
-
-
-  // 3. receiving the response
-  request.send()
 }
-
-
 // 6. calling the callback function
 
 // 7. callback taking an argument ( err - for error catching -- data - for data catching)
-getData((err, data) => {
-  console.log('callback fired');
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-})
+// 8. callback hell for getting data one after another below is the callback hell
+
+// as getData is returning a promise we dont need this
+
+// getData('./todos/messi.json', (err, data) => {
+//   console.log('Getting Messi Data - 1');
+//   console.log(data);
+//   getData('./todos/suarez.json', (err, data) => {
+//     console.log('Getting Suarez Data - 2');
+//     console.log(data);
+//     getData('./todos/neymar.json', (err, data) => {
+//       console.log('Getting Neymar Data - 3');
+//       console.log(data);
+//     })
+//   })
+// })
+
+
+// using promise chaining to get multiple data request
+getData('./todos/messi.json')
+  .then(data => {
+    console.log('Promise 1 resolved',data)
+    return getData('./todos/suarez.json')
+  })
+  .then(data => {
+    console.log('Promise 2 resolved', data);
+    return getData('./todos/neymar.json')
+  })
+  .then(data => {
+    console.log('Promise 3 resolved', data);
+  })
+  .catch(error => console.error(error))
+
+
+// // promise example
+
+// // creating new promise which takes two arguments - one is resolve and another one reject
+// const getSomething = () => {
+//   return new Promise((resolve, reject) => {
+//     resolve('some data')
+//     // reject('some data')
+//   })
+// }
+
+// // calling the getSomething function and chaining the then method
+// getSomething()
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err))
